d14764a4d3a10b98a82bfc6cb2a035b8c23706d8
Dennis Bogers <dennisbogerskorsit@gmail.com>
2019-06-07 13:31:51 +0200

    # Fix file formatting with eslint.

--- src/functions/translate.ts
+++ src/functions/translate.ts
@@ -1,8 +1,7 @@
-
 import { TranslationParameters } from '../interfaces/translationParameters';
 import { TranslationResponse } from '../interfaces/translationResponse';
 import * as querystring from 'query-string';
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 
 /**
  * Translate a string into another language using the DeepL API.
@@ -12,6 +11,7 @@ import fetch from "node-fetch";
 export async function translate(params: TranslationParameters): Promise<TranslationResponse> {
   return await fetch(`https://api.deepl.com/v2/translate?${querystring.stringify(params)}`)
     .then(res => {
-      return res.json()
-    }).catch(error => console.error(error));
+      return res.json();
+    })
+    .catch(error => console.error(error));
 }

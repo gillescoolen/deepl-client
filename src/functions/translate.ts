cc57bd26066f3e9d74d4ac5ff8eaa9ef557e8cce
Gilles Coolen <gilleswhc@gmail.com>
2019-06-07 23:47:32 +0200

    Remove callback

--- src/functions/translate.ts
+++ src/functions/translate.ts
@@ -6,12 +6,9 @@ import fetch from 'node-fetch';
 /**
  * Translate a string into another language using the DeepL API.
  * @property {TranslationParameters} params The parameters you can send to configure DeepL.
- * @returns {TranslationResponse} An array of translated text.
+ * @returns {Promise<TranslationResponse>} An array of translated text.
  */
 export async function translate(params: TranslationParameters): Promise<TranslationResponse> {
-  return await fetch(`https://api.deepl.com/v2/translate?${querystring.stringify(params)}`)
-    .then(res => {
-      return res.json();
-    })
-    .catch(error => console.error(error));
+  const response = await fetch(`https://api.deepl.com/v2/translate?${querystring.stringify(params)}`)
+  return response.json();
 }

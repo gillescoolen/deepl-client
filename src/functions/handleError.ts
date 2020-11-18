b1cdb3fad46922fa3cd775c49dafc2a36a95dbe2
Gilles <gilleswhc@gmail.com>
2020-11-18 14:17:42 +0100

    Actually throw error in handleError

--- src/functions/handleError.ts
+++ src/functions/handleError.ts
@@ -5,33 +5,25 @@ import { Response } from 'node-fetch';
  * @param response The response from the DeepL API.
  */
 export async function handleError(response: Response): Promise<void> {
-  switch (response.status) {
-    case 400:
-      console.error('400 - Bad request. Please check error message and your parameters.');
-      break;
-    case 403:
-      console.error('403 - Authorization failed. Please supply a valid auth_key parameter.');
-      break;
-    case 404:
-      console.error('404 - The requested resource could not be found.');
-      break;
-    case 413:
-      console.error('413 - The request size exceeds the limit.');
-      break;
-    case 429:
-      console.error('429 - Too many requests. Please wait and resend your request.');
-      break;
-    case 456:
-      console.error('456 - Quota exceeded. The character limit has been reached.');
-      break;
-    case 503:
-      console.error('503 - Resource currently unavailable. Try again later.');
-      break;
-    default:
-      console.error('5XX - Internal Error. Check the docs for more information.');
-      break;
-  }
-
   const json = await response.json();
   if (json.message) console.error(json.message);
+
+  switch (response.status) {
+    case 400:
+      throw new Error('400 - Bad request. Please check error message and your parameters.');
+    case 403:
+      throw new Error('403 - Authorization failed. Please supply a valid auth_key parameter.');
+    case 404:
+      throw new Error('404 - The requested resource could not be found.');
+    case 413:
+      throw new Error('413 - The request size exceeds the limit.');
+    case 429:
+      throw new Error('429 - Too many requests. Please wait and resend your request.');
+    case 456:
+      throw new Error('456 - Quota exceeded. The character limit has been reached.');
+    case 503:
+      throw new Error('503 - Resource currently unavailable. Try again later.');
+    default:
+      throw new Error('5XX - Internal Error. Check the docs for more information.');
+  }
 }
